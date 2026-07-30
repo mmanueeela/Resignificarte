9ebbb4872f30236f3c5956cb86c7a2d6048371bb
Ferraann <126678093+mmanueeela@users.noreply.github.com>
2026-07-30 13:39:48 +0200

    pruebas

--- php/contrasena_olvidada.php
+++ php/contrasena_olvidada.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header('Content-Type: application/json');
 
 // 1. Incluimos la conexión (como están en la misma carpeta 'php')

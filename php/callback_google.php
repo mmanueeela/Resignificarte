914c48615b9d01005f39f40df2053f128c5ded11
Ferraann <126678093+mmanueeela@users.noreply.github.com>
2026-07-30 21:33:08 +0200

    prueba a cambio de correo

--- php/callback_google.php
+++ php/callback_google.php
@@ -135,7 +135,7 @@ if (isset($_GET['code'])) {
 
                 $cabeceras  = 'MIME-Version: 1.0' . "\r\n";
                 $cabeceras .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-                $cabeceras .= 'From: Resignificarte <hola@mzazzar.upv.edu.es>' . "\r\n";
+                $cabeceras .= 'From: Resignificarte <hola@mzazzar.epsg.upv.es>' . "\r\n";
 
                 mail($email, $asunto, $mensaje, $cabeceras);
 
--- php/procesar_registro.php
+++ php/procesar_registro.php
@@ -95,7 +95,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $cabeceras  = 'MIME-Version: 1.0' . "\r\n";
         $cabeceras .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-        $cabeceras .= 'From: Resignificarte <hola@mzazzar.upv.edu.es>' . "\r\n";
+        $cabeceras .= 'From: Resignificarte <hola@mzazzar.epsg.upv.es>' . "\r\n";
 
         mail($email, $asunto, $mensaje, $cabeceras);
         // ==========================================

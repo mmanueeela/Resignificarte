80f445b1b0946632b54a462c8655febb60966f6b
Ferraann <126678093+mmanueeela@users.noreply.github.com>
2026-08-08 20:46:01 +0200

    prueba solucion header obras cuando logeado

--- contacto.php
+++ contacto.php
@@ -13,7 +13,7 @@ require_once 'php/logicaNegocio/cargar_usuario_header.php';
     <link rel="icon" href="favicon.ico" type="image/x-icon">
     <script src="js/retardo_cambio_pagina.js" defer></script>
     <script src="js/contacto.js" defer></script>
-    <script src="js/abrir_popup_homepage_usuario_registrado.js" defer></script>
+    <script src="js/abrir_popup_header.js" defer></script>
 </head>
 <body>
 <main>

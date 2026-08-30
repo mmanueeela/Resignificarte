7f0fa02765182a51664ce57505712952123f6ed3
Ferraann <126678093+mmanueeela@users.noreply.github.com>
2026-08-30 23:07:12 +0200

    Update login_vr.php

--- php/login_vr.php
+++ php/login_vr.php
@@ -5,17 +5,17 @@ $contrasena = "elena2009plesk@";
 $base_datos = "resignificarte";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) die("Conexion fallida");
+
+// Si falla la conexión a la base de datos, devuelve un error específico
+if ($conn->connect_error) {
+    die("DB_ERROR");
+}
 
 if (isset($_POST['telefono'])) {
     $telefono = $conn->real_escape_string($_POST['telefono']);
 
-    // Cruzamos las dos tablas para obtener el nombre si el teléfono coincide
-    $sql = "SELECT u.nombre 
-            FROM usuarios_credenciales uc 
-            INNER JOIN usuarios u ON uc.usuario_id = u.id 
-            WHERE uc.telefono = '$telefono'";
-
+    // Unimos las dos tablas para obtener el nombre a partir del teléfono
+    $sql = "SELECT u.nombre FROM usuarios u INNER JOIN usuarios_credenciales uc ON u.id = uc.usuario_id WHERE uc.telefono = '$telefono'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -24,6 +24,8 @@ if (isset($_POST['telefono'])) {
     } else {
         echo "ERROR";
     }
+} else {
+    echo "NO_POST";
 }
 $conn->close();
 ?>

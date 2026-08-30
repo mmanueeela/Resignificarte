32e8bc3b1882d19c1f46675c1014202b7f09f397
Ferraann <126678093+mmanueeela@users.noreply.github.com>
2026-08-30 23:10:03 +0200

    prueba conocer errores

--- php/login_vr.php
+++ php/login_vr.php
@@ -1,4 +1,10 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 $servidor   = "localhost";
 $usuario    = "manuela";
 $contrasena = "elena2009plesk@";
